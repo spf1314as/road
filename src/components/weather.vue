113535c1e99aad041c5bbf6b39c24fce8fe60e82
shenpengfei <shenpengfei@xiaomi.com>
2019-08-31 14:58:49 +0800

    修改

--- src/components/weather.vue
+++ src/components/weather.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div class="trafficBox">
+     <div class="walkerBox">
+       人步行的标志
+     </div>
+     <div class="bikeBox">
+       骑自行车的标志
+     </div>
+    </div>
     <!-- <h2>Essential Links</h2> -->
 
   </div>
 </template>
 
 <script>
+import echarts from 'echarts';
 export default {
   name: 'HelloWorld',
   data () {
     return {
+      // msg: 'Welcome to Your Vue.js App'
       msg: 'Welcome to Your Vue.js App'
     }
   }
@@ -19,6 +29,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang = "less">
+.walkerBox
+{
+   border: 1px solid red;
+   height: 100px;
+}
+.bikeBox{
+  border: 1px solid red;
+  height: 100px;
+}
+
 h1, h2 {
   font-weight: normal;
 }
